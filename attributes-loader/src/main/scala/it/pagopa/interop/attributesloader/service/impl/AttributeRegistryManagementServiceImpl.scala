d8002886a976df86e446218e682f2d78a9a49694
Antonio Gelameris <toniogela89@gmail.com>
2022-03-31 11:11:06 +0200

    Fixing compilation

--- attributes-loader/src/main/scala/it/pagopa/interop/attributesloader/service/impl/AttributeRegistryManagementServiceImpl.scala
+++ attributes-loader/src/main/scala/it/pagopa/interop/attributesloader/service/impl/AttributeRegistryManagementServiceImpl.scala
@@ -1,5 +1,6 @@
 package it.pagopa.interop.attributesloader.service.impl
 
+import scala.concurrent.{ExecutionContext, Future}
 import it.pagopa.interop.attributeregistrymanagement.client.api.AttributeApi
 import it.pagopa.interop.attributeregistrymanagement.client.invoker.{ApiRequest, BearerToken}
 import it.pagopa.interop.attributesloader.service.{
@@ -8,15 +9,19 @@ import it.pagopa.interop.attributesloader.service.{
 }
 import org.slf4j.{Logger, LoggerFactory}
 
-import scala.concurrent.Future
-
-final case class AttributeRegistryManagementServiceImpl(invoker: AttributeRegistryManagementInvoker, api: AttributeApi)
-    extends AttributeRegistryManagementService {
+final case class AttributeRegistryManagementServiceImpl(invoker: AttributeRegistryManagementInvoker, api: AttributeApi)(
+  implicit ec: ExecutionContext
+) extends AttributeRegistryManagementService {
 
   implicit val logger: Logger = LoggerFactory.getLogger(this.getClass)
 
   override def loadCertifiedAttributes(bearerToken: String): Future[Unit] = {
-    val request: ApiRequest[Unit] = api.loadCertifiedAttributes()(BearerToken(bearerToken))
+    // If we will have different jobs in the future we might consider using xCorrelationId
+    // to log the specific job name that performed this call
+    val request: ApiRequest[Unit] = api
+      .loadCertifiedAttributes(xCorrelationId = s"interop-be-jobs-${System.currentTimeMillis()}")(
+        BearerToken(bearerToken)
+      )
     invoker.invoke(request, s"Loading certificated attributes")
   }
 }
