4dad47c1931d9b007906bdf239e87b7c1c05727d
Ubaldo Pescatore <P3trur0@users.noreply.github.com>
2022-05-10 10:46:46 +0200

    PIN-1400 added Vault transit support (#29)

--- attributes-loader/src/main/scala/it/pagopa/interop/attributesloader/Main.scala
+++ attributes-loader/src/main/scala/it/pagopa/interop/attributesloader/Main.scala
@@ -6,12 +6,12 @@ import it.pagopa.interop.attributesloader.service.AttributeRegistryManagementInv
 import it.pagopa.interop.attributesloader.service.impl.AttributeRegistryManagementServiceImpl
 import it.pagopa.interop.attributesloader.system.{ApplicationConfiguration, classicActorSystem, executionContext}
 import it.pagopa.interop.commons.jwt._
-import it.pagopa.interop.commons.jwt.model.RSA
 import it.pagopa.interop.commons.jwt.service.InteropTokenGenerator
 import it.pagopa.interop.commons.jwt.service.impl.DefaultInteropTokenGenerator
 import it.pagopa.interop.commons.utils.TypeConversions.TryOps
-import it.pagopa.interop.commons.vault.service.VaultService
-import it.pagopa.interop.commons.vault.service.impl.{DefaultVaultClient, DefaultVaultService}
+import it.pagopa.interop.commons.vault.VaultClientConfiguration
+import it.pagopa.interop.commons.vault.service.VaultTransitService
+import it.pagopa.interop.commons.vault.service.impl.VaultTransitServiceImpl
 import org.slf4j.{Logger, LoggerFactory}
 
 import scala.concurrent.Future
@@ -20,10 +20,6 @@ import scala.util.{Failure, Success, Try}
 //shuts down the actor system in case of startup errors
 case object ErrorShutdown   extends CoordinatedShutdown.Reason
 case object SuccessShutdown extends CoordinatedShutdown.Reason
-
-trait VaultServiceDependency {
-  val vaultService: VaultService = new DefaultVaultService with DefaultVaultClient.DefaultClientInstance
-}
 
 trait AttributeRegistryManagementDependency {
   val attributeRegistryManagementApi: AttributeApi = AttributeApi(
@@ -34,35 +30,37 @@ trait AttributeRegistryManagementDependency {
     AttributeRegistryManagementServiceImpl(AttributeRegistryManagementInvoker(), attributeRegistryManagementApi)
 }
 
-object Main extends App with VaultServiceDependency with AttributeRegistryManagementDependency {
+object Main extends App with AttributeRegistryManagementDependency {
 
   implicit val logger: Logger = LoggerFactory.getLogger(this.getClass)
 
   logger.info("Loading attributes data...")
 
   lazy val jwtConfig: JWTInternalTokenConfig = JWTConfiguration.jwtInternalTokenConfig
+  val vaultService: VaultTransitService      = new VaultTransitServiceImpl(VaultClientConfiguration.vaultConfig)
 
   val interopTokenGenerator: Try[InteropTokenGenerator] =
-    Try(new DefaultInteropTokenGenerator with PrivateKeysHolder {
-      override val RSAPrivateKeyset: Map[KID, SerializedKey] =
-        vaultService.readBase64EncodedData(ApplicationConfiguration.rsaPrivatePath)
-      override val ECPrivateKeyset: Map[KID, SerializedKey]  =
-        Map.empty
-    })
+    Try(
+      new DefaultInteropTokenGenerator(
+        vaultService,
+        new PrivateKeysKidHolder {
+          override val RSAPrivateKeyset: Set[KID] = ApplicationConfiguration.rsaKeysIdentifiers
+          override val ECPrivateKeyset: Set[KID]  = ApplicationConfiguration.ecKeysIdentifiers
+        }
+      )
+    )
 
   val result: Future[Unit] = for {
     tokenGenerator <- interopTokenGenerator.toFuture
     m2mToken       <- tokenGenerator
       .generateInternalToken(
-        jwtAlgorithmType = RSA,
         subject = jwtConfig.subject,
         audience = jwtConfig.audience.toList,
         tokenIssuer = jwtConfig.issuer,
         secondsDuration = jwtConfig.durationInSeconds
       )
-      .toFuture
     _ = logger.info("M2M Token obtained")
-    _ <- attributeRegistryManagementService.loadCertifiedAttributes(m2mToken)
+    _ <- attributeRegistryManagementService.loadCertifiedAttributes(m2mToken.serialized)
   } yield ()
 
   result.onComplete {
