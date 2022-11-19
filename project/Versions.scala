ed336558215a7e7bd345c226575a99453732e110
Scala Steward <scala-steward@pagopa.it>
2022-11-19 06:50:54 +0000

    Update logback-classic to 1.4.5 in 1.0.x

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@ object Versions {
   lazy val akkaVersion    = "2.6.20"
   lazy val catsVersion    = "2.8.0"
   lazy val jacksonVersion = "2.11.4" // This cannot be updated yet because akka-serialization use 2.11.x version
-  lazy val logbackVersion = "1.4.4"
+  lazy val logbackVersion = "1.4.5"
   lazy val mongodbScalaDriverVersion = "4.7.2"
   lazy val munitVersion              = "0.7.29"
 }
