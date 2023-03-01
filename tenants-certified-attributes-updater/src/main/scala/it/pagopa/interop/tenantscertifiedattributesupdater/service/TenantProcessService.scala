90bd85dfe043db59ca916677d9bdc66fab9a46f7
Riccardo Torsoli <122275960+nttdata-rtorsoli@users.noreply.github.com>
2023-03-01 12:36:12 +0100

    PIN-2693 Il job di caricamento attributi deve revocare attributi non più assegnati (#92)
    * PIN-2693 activate revokations
    
    * PIN-2693 Resolve PR requests
    
    * PIN-2693 Resolve PR requests added call to traverseWithLatch
    
    ---------
    
    Co-authored-by: Alessio Gallitano <25105748+galales@users.noreply.github.com>

--- tenants-certified-attributes-updater/src/main/scala/it/pagopa/interop/tenantscertifiedattributesupdater/service/TenantProcessService.scala
+++ tenants-certified-attributes-updater/src/main/scala/it/pagopa/interop/tenantscertifiedattributesupdater/service/TenantProcessService.scala
@@ -1,11 +1,17 @@
 package it.pagopa.interop.tenantscertifiedattributesupdater.service
 
 import it.pagopa.interop.tenantprocess.client.model.InternalTenantSeed
+import it.pagopa.interop.tenantmanagement.model.tenant.PersistentExternalId
+import it.pagopa.interop.tenantscertifiedattributesupdater.util.AttributeInfo
 
 import scala.concurrent.Future
 
 trait TenantProcessService {
   def upsertTenant(bearerToken: String)(seed: InternalTenantSeed)(implicit
     contexts: Seq[(String, String)]
+  ): Future[Unit]
+
+  def revokeCertifiedAttribute(bearerToken: String)(externalId: PersistentExternalId, attributeInfo: AttributeInfo)(
+    implicit contexts: Seq[(String, String)]
   ): Future[Unit]
 }
