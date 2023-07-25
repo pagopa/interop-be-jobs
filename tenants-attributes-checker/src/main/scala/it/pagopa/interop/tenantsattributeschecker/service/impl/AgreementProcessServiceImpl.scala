f7f60611998550b953e62bd056ee5f1aae94d90d
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2023-07-25 16:01:47 +0200

    PIN-3655: Update computeAgreementState signature

--- tenants-attributes-checker/src/main/scala/it/pagopa/interop/tenantsattributeschecker/service/impl/AgreementProcessServiceImpl.scala
+++ tenants-attributes-checker/src/main/scala/it/pagopa/interop/tenantsattributeschecker/service/impl/AgreementProcessServiceImpl.scala
@@ -4,6 +4,7 @@ import akka.actor.typed.ActorSystem
 import com.typesafe.scalalogging.{Logger, LoggerTakingImplicit}
 import it.pagopa.interop.agreementprocess.client.api.{AgreementApi, EnumsSerializers}
 import it.pagopa.interop.agreementprocess.client.invoker.{ApiInvoker, BearerToken}
+import it.pagopa.interop.agreementprocess.client.model.{CompactTenant, ComputeAgreementStatePayload}
 import it.pagopa.interop.commons.logging.{CanLogContextFields, ContextFieldsToLog}
 import it.pagopa.interop.commons.utils.TypeConversions.EitherOps
 import it.pagopa.interop.commons.utils.extractHeaders
@@ -22,7 +23,7 @@ final case class AgreementProcessServiceImpl(blockingEc: ExecutionContextExecuto
   implicit val logger: LoggerTakingImplicit[ContextFieldsToLog] =
     Logger.takingImplicit[ContextFieldsToLog](this.getClass)
 
-  override def computeAgreementsByAttribute(consumerId: UUID, attributeId: UUID)(implicit
+  override def computeAgreementsByAttribute(attributeId: UUID, consumer: CompactTenant)(implicit
     contexts: Seq[(String, String)]
   ): Future[Unit] = {
     implicit val ec: ExecutionContext = blockingEc
@@ -31,12 +32,11 @@ final case class AgreementProcessServiceImpl(blockingEc: ExecutionContextExecuto
       (bearerToken, correlationId, ip) <- extractHeaders(contexts).toFuture
       request = api.computeAgreementsByAttribute(
         xCorrelationId = correlationId,
-        consumerId = consumerId,
-        attributeId = attributeId,
+        computeAgreementStatePayload = ComputeAgreementStatePayload(attributeId, consumer),
         xForwardedFor = ip
       )(BearerToken(bearerToken))
       result <- invoker
-        .invoke(request, s"Agreements state compute triggered for Tenant $consumerId and Attribute $attributeId")
+        .invoke(request, s"Agreements state compute triggered for Tenant ${consumer.id} and Attribute $attributeId")
     } yield result
   }
 }
