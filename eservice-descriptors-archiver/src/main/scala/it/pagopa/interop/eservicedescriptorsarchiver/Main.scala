a39060ff6a09a75124be91b338ee0ea698599764
Stefano Perazzolo <13318704+beetlecrunch@users.noreply.github.com>
2023-09-07 09:21:31 +0200

    eservice-descriptors-eservice: remove Await

--- eservice-descriptors-archiver/src/main/scala/it/pagopa/interop/eservicedescriptorsarchiver/Main.scala
+++ eservice-descriptors-archiver/src/main/scala/it/pagopa/interop/eservicedescriptorsarchiver/Main.scala
@@ -1,7 +1,6 @@
 package it.pagopa.interop.eservicedescriptorsarchiver
 
-import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future}
+import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
 object Main extends App with Dependencies {
@@ -14,13 +13,11 @@ object Main extends App with Dependencies {
 
   private def executionLoop(): Future[Unit] = execution.flatMap(_ => executionLoop())
 
-  private val init: Future[Unit] = executionLoop()
+  executionLoop()
     .andThen {
       case Failure(e) => logger.error("Eservice version archiver job failed with exception", e)
       case Success(_) => logger.info("Completed eservice version archiver job")
     }
-    .andThen(_ => blockingThreadPool.shutdown())
-
-  Await.ready(init, Duration.Inf)
+    .andThen(_ => blockingThreadPool.shutdown()): Unit
 
 }
