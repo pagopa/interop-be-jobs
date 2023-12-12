ba5e2ba44d076a3b40a95673491063ef4881b7bf
Stefano Perazzolo <13318704+beetlecrunch@users.noreply.github.com>
2023-12-12 11:21:35 +0100

    fix metrics-report-generator descriptors query (#168)

--- metrics-report-generator/src/main/scala/it/pagopa/interop/metricsreportgenerator/util/models.scala
+++ metrics-report-generator/src/main/scala/it/pagopa/interop/metricsreportgenerator/util/models.scala
@@ -1,13 +1,14 @@
 package it.pagopa.interop.metricsreportgenerator.util.models
 
-import java.time.LocalDate
+import cats.syntax.all._
 import it.pagopa.interop.commons.utils.errors.GenericComponentErrors.GenericError
-import scala.util._
+import it.pagopa.interop.metricsreportgenerator.util.Errors.ChecksumNotFound
 import spray.json.DefaultJsonProtocol._
 import spray.json._
-import cats.syntax.all._
-import java.time.Instant
-import java.time.ZoneId
+
+import java.time.{Instant, LocalDate, ZoneId}
+import scala.concurrent.Future
+import scala.util._
 
 final case class Agreement(
   activationDate: Option[String],
@@ -36,12 +37,15 @@ final case class Descriptor(
   producer: String,
   descriptorId: String,
   state: String,
-  checksum: String
+  checksum: Option[String]
 ) {
   def isActive: Boolean = state == "Suspended" || state == "Published" || state == "Deprecated"
 
-  def toMetric: MetricDescriptor =
-    MetricDescriptor(name, createdAt, producerId, producer, descriptorId, state, checksum)
+  def toMetric: Future[MetricDescriptor] = checksum
+    .fold[Future[MetricDescriptor]](Future.failed(ChecksumNotFound(descriptorId)))(chs =>
+      Future.successful(MetricDescriptor(name, createdAt, producerId, producer, descriptorId, state, chs))
+    )
+
 }
 
 object Descriptor {
